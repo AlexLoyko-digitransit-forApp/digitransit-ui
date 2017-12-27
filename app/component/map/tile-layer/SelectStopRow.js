f17456656e2aacb7405832890ab271ce415ed9a4
Alex Loyko <alex.loyko96@gmail.com>
2017-12-27 15:22:26 -0500

    Added color to the stop-rows (circle-clicks)

--- app/component/map/tile-layer/SelectStopRow.js
+++ app/component/map/tile-layer/SelectStopRow.js
@@ -4,15 +4,13 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import uniqBy from 'lodash/uniqBy';
 import reject from 'lodash/reject';
-
-import RouteDestination from '../../RouteDestination';
-import routeCompare from '../../../util/route-compare';
 import ComponentUsageExample from '../../ComponentUsageExample';
 
 function getName(pattern) {
   if (pattern.shortName) {
     return (
       <span
+        style={{ color: pattern.color ? `#${pattern.color}` : null }}
         key={pattern.shortName}
         className={`${pattern.type.toLowerCase()} vehicle-number`}
       >
@@ -24,21 +22,21 @@ function getName(pattern) {
 }
 
 function SelectStopRow(props) {
-  const patternData = JSON.parse(props.patterns).sort(routeCompare);
+  const patternData = JSON.parse(props.patterns);
   const patterns = [];
 
   patterns.push(
     <div key="first" className="route-detail-text">
       <span
+        style={{
+          color: patternData[0].color ? `#${patternData[0].color}` : null
+        }}
         className={`${patternData[0].type.toLowerCase()} vehicle-number no-padding`}
       >
         {patternData[0].shortName}
       </span>
       {'\u00a0'}
-      <RouteDestination
-        mode={patternData[0].type}
-        destination={patternData[0].headsign}
-      />
+      {patternData[0].headsign}
     </div>,
   );
 
